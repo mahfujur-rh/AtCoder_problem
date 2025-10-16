973b7f185d2f9373b9d2ad9f1b4dc8d5b86a38a8
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-16 21:26:09 +0600

    Create problem-3.cpp

---
+++ problem-3.cpp
@@ -0,0 +1,16 @@
+//https://atcoder.jp/contests/abc326/tasks/abc326_a
+
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int x,y;cin>>x>>y;
+    if (y>=x && y-x<=2 || y<=x && x-y<=3)  
+    {
+        cout <<"Yes"<<endl;
+
+    }
+    else cout<<"No"<<endl;
+    
+    
+ return 0;
+}
