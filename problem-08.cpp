715ffd1ddc64ce2b563827d09c7c13ee137c406a
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-01 20:15:15 +0600

    Create problem-08.cpp

---
+++ problem-08.cpp
@@ -0,0 +1,14 @@
+//https://atcoder.jp/contests/abc430/tasks/abc430_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	
+	if(c>=a && d<b){
+	    cout<<"Yes"<<endl;
+	}
+	else cout<<"No"<<endl;
+}
