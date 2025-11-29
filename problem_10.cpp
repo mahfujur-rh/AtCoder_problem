02f0011ca2daf410efd61aef226887f435c99f11
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-29 18:42:25 +0600

    Create problem_10.cpp

---
+++ problem_10.cpp
@@ -0,0 +1,29 @@
+//https://atcoder.jp/contests/abc237/tasks/abc237_b?lang=en
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int h, w;
+    cin >> h >> w;
+
+    int arr[h][w];
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int j = 0; j < w; j++) {
+        for (int i = 0; i < h; i++) {
+            cout << arr[i][j]<<" ";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
